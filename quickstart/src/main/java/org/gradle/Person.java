5c3a6adcf58c29cf6a79601bcd8cc47627c9584d
melvicaballero <melvi.caballero@gmail.com>
2019-07-03 07:25:21 -0400

    checkstyle

--- quickstart/src/main/java/org/gradle/Person.java
+++ quickstart/src/main/java/org/gradle/Person.java
@@ -2,14 +2,25 @@ package org.gradle;
 
 import org.apache.commons.collections.list.GrowthList;
 
+/**
+ * Person class, with name as atribute.
+ */
 public class Person {
     private final String name;
 
-    public Person(String name) {
+    /**
+     * Method name constructor of the class.
+     * @param name person name.
+     */
+    public Person(final String name) {
         this.name = name;
         new GrowthList();
     }
 
+    /**
+     * Method getName,  get the person name.
+     * @return person name.
+     */
     public String getName() {
         return name;
     }
--- quickstart/src/test/java/org/gradle/PersonTest.java
+++ quickstart/src/test/java/org/gradle/PersonTest.java
@@ -1,11 +1,17 @@
 package org.gradle;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+/**
+ * Class PersonTest test for the class person.
+ */
 public class PersonTest {
+    /**
+     * Test for getName method.
+     */
     @Test
-    public void canConstructAPersonWithAName() {
+        public void canConstructAPersonWithAName() {
         Person person = new Person("Larry");
         assertEquals("Larry", person.getName());
     }
